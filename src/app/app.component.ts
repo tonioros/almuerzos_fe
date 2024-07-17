fb9104b58c1fb41846706405863ec28d9db32c76
Antonio Xocoy Rosales <anxrosales@gmail.com>
2024-07-16 20:54:12 -0600

    feat: AppComponent
    * Add event to go to page selected using list elements
    * Change favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {MatDrawerMode} from "@angular/material/sidenav";
+import {Component} from '@angular/core';
+import {MatDrawerMode, MatSidenav} from "@angular/material/sidenav";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -11,7 +11,15 @@ export class AppComponent {
   title = 'almuerzos-gratis';
   mode?: MatDrawerMode;
 
+  constructor(private router: Router) {
+  }
+
   showInfo(link: any) {
 
+  }
+
+  goTo(s: string, drawer: MatSidenav) {
+    drawer.close();
+    this.router.navigate([s]);
   }
 }
